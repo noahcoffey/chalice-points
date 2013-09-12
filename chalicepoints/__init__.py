0ba4698e0cc72a019117f18b224312dcd624e8ee
Brandon Peters <brandon.peters@gmail.com>
2013-09-12 03:41:42 +0000

    Modularized everything and changed to Flask Blueprints.

---
+++ chalicepoints/__init__.py
@@ -0,0 +1,77 @@
+import os, sys, string
+import json
+import redis
+
+from flask import Flask, redirect, url_for, abort, jsonify
+from flask.ext.login import LoginManager, login_user
+from flask.ext.openid import OpenID
+
+from chalicepoints.models.user import User
+
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+app = Flask(__name__, static_folder=os.path.join(PROJECT_ROOT, 'public'),
+        static_url_path='/public')
+
+app.config.from_object('chalicepoints.config.DefaultConfig')
+app.config['SECRET_KEY'] = os.getenv('APP_SECRET_KEY', app.config['SECRET_KEY'])
+
+if app.config['SECRET_KEY'] is None:
+    abort(500)
+
+# Flask-OpenID
+open_id = OpenID()
+open_id.init_app(app)
+
+# Flask-Login
+login_manager = LoginManager()
+login_manager.login_view = 'site.login'
+login_manager.init_app(app)
+
+# Redis
+redis_url = os.getenv('REDISTOGO_URL', app.config['REDIS_URL'])
+r = redis.from_url(redis_url)
+
+def register_blueprint(app):
+    from chalicepoints.views.site import site
+    app.register_blueprint(site)
+
+    from chalicepoints.views.api import api
+    app.register_blueprint(api)
+
+register_blueprint(app)
+
+@login_manager.user_loader
+def load_user(id):
+    from chalicepoints.models.user import User
+
+    user_json = r.hget('openid', id)
+    if user_json:
+        u = json.loads(user_json)
+        return User(u['url'], u['email'], u['name'])
+    else:
+        return None
+
+@open_id.after_login
+def after_login(response):
+    from chalicepoints.models.user import User
+
+    email = string.lower(response.email)
+
+    emails = User.get_user_emails()
+    if email not in emails:
+        abort(401)
+
+    user = User(response.identity_url, email, emails[email])
+    user_json = user.to_json()
+    r.hset('openid', user.url, user_json)
+    login_user(user)
+
+    return redirect(url_for('site.index'))
+
+if __name__ == "__main__":
+    port = 9896
+    if len(sys.argv) == 2:
+        port = int(sys.argv[1])
+
+    app.run(host='0.0.0.0', port=port, debug=True)
---
+++ chalicepoints/models/event.py
@@ -0,0 +1,126 @@
+import json
+from datetime import datetime
+
+from flask import abort, jsonify
+from flask.ext.login import current_user
+
+import chalicepoints
+from chalicepoints.models.base import BaseModel
+from chalicepoints.models.user import User
+
+class Event(BaseModel):
+    @staticmethod
+    def do_get_events():
+        events = {}
+
+        users = User.get_users()
+        for name in users:
+            events[name] = Event.get_events(name)
+
+        return jsonify(success=1, events=events)
+
+    @staticmethod
+    def do_post_events(data):
+        source = current_user.name
+        target = data['target'].encode('ascii')
+        amount = max(min(5, data['amount']), 1)
+
+        if target == current_user.name:
+            abort(403)
+
+        users = User.get_user_names()
+        if target not in users:
+            abort(403)
+
+        message = 'None'
+        if 'message' in data and data['message']:
+            message = data['message'].encode('ascii')
+
+        eventDate = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+        Event.add_event(source, target, eventDate, amount, message)
+
+        return jsonify(success=1)
+
+    @staticmethod
+    def get_events(name, deleted=None):
+        userKey = Event.to_key(name)
+        eventsKey = 'cpEvents' + userKey
+
+        events = []
+        if chalicepoints.r.exists(eventsKey):
+            eventsLen = chalicepoints.r.llen(eventsKey)
+            for idx in range(eventsLen):
+                eventJSON = chalicepoints.r.lindex(eventsKey, idx)
+                event = json.loads(eventJSON)
+
+                if not deleted and '__deleted' in event:
+                    continue
+
+                events.append(event)
+
+        return events
+
+    @staticmethod
+    def add_event(source, target, eventDate, amount, message):
+        if source == target:
+            return False
+
+        sourceKey = Event.to_key(source)
+        givenKey = 'cpEvents' + sourceKey
+        given = {
+            'type': 'give',
+            'user': target,
+            'amount': amount,
+            'date': eventDate,
+            'message': message,
+        }
+        chalicepoints.r.lpush(givenKey, json.dumps(given))
+
+        targetKey = Event.to_key(target)
+        receivedKey = 'cpEvents' + targetKey
+        received = {
+            'type': 'receive',
+            'user': source,
+            'amount': amount,
+            'date': eventDate,
+            'message': message,
+        }
+        chalicepoints.r.lpush(receivedKey, json.dumps(received))
+
+        Event.update_hipchat(source, target, amount, message)
+
+    '''
+    @staticmethod
+    def remove_event(source, target, date):
+        source = source.encode('ascii')
+        target = target.encode('ascii')
+        date = date.encode('ascii')
+
+        found = False
+
+        sourceEvents = Event.get_events(source)
+        for idx in range(len(sourceEvents)):
+            event = sourceEvents[idx]
+            if event['user'] == target and event['date'] == date:
+                found = True
+                Event.set_deleted_flag(source, idx, event, True)
+
+        targetEvents = Event.get_events(target)
+        for idx in range(len(targetEvents)):
+            event = targetEvents[idx]
+            if event['user'] == source and event['date'] == date:
+                found = True
+                setDeletedFlag(source, idx, event, True)
+
+        return found
+
+    @staticmethod
+    def set_deleted_flag(key, idx, event, deleted=True):
+        if deleted:
+            event['__deleted'] = 1
+        else:
+            event.pop('__deleted', None)
+
+        chalicepoints.r.lset(key, idx, json.dumps(event))
+    '''
