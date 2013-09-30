64243d6f711be375d9b149bf96ba9457b0b97f9f
Brandon Peters <brandon@formstack.com>
2013-09-30 10:35:05 -0400

    Added lookup support for __init__. Added get_user() and get_user_by_email(). Added max_points, disabled, and gravatar properties.

--- chalicepoints/models/user.py
+++ chalicepoints/models/user.py
@@ -6,10 +6,15 @@ import chalicepoints
 from chalicepoints.models.base import BaseModel
 
 class User(BaseModel, UserMixin):
-    def __init__(self, url, email, name):
+    def __init__(self, url, email):
+        user = User.get_user_by_email(email)
+
         self.url = url
         self.email = email
-        self.name = name
+        self.name = user['name']
+        self.gravatar = user['gravatar']
+        self.max_points = user['max_points']
+        self.disabled = user['disabled']
 
     def get_id(self):
         return self.url
@@ -18,10 +23,13 @@ class User(BaseModel, UserMixin):
         d = {
             'email': self.email,
             'name' : self.name,
+            'gravatar': self.gravatar,
         }
 
         if not for_public:
             d['url'] = self.url
+            d['max_points'] = self.max_points
+            d['disabled'] = self.disabled
 
         return json.dumps(d)
 
@@ -72,3 +80,27 @@ class User(BaseModel, UserMixin):
                 users[user['name']] = user
 
         return users
+
+    @staticmethod
+    def get_user(name):
+        user_key = User.to_key(name)
+
+        user = None
+        if chalicepoints.r.hexists('cpUsers', user_key):
+            user_json = chalicepoints.r.hget('cpUsers', user_key)
+            user = json.loads(user_json)
+
+        return user
+
+    @staticmethod
+    def get_user_by_email(email):
+        user = None
+        user_dict = User.get_user_dict()
+        if user_dict != None:
+            for user_key in user_dict:
+                user_json = user_dict[user_key]
+                user_obj = json.loads(user_json)
+                if user_obj['email'] == email:
+                    user = user_obj
+
+        return user
