6c376fdc8f62c79178b11f27bb5b1901c1398e0a
Brandon Peters <brandon.peters@gmail.com>
2014-02-19 03:54:53 +0000

    Killed google website tools tracking entry. Changed to current_app

--- chalicepoints/views/site.py
+++ chalicepoints/views/site.py
@@ -1,12 +1,13 @@
+import os, sys
+
 from flask import Flask, request, redirect, url_for, \
     abort, render_template, jsonify, send_from_directory, \
-    Response, g, Blueprint
+    Response, g, Blueprint, current_app
 
 from flask.ext.login import login_required, current_user, logout_user
 
 import chalicepoints
-from chalicepoints import open_id
-from chalicepoints import login_manager
+from chalicepoints import open_id, login_manager
 
 site = Blueprint('site', __name__)
 
@@ -19,32 +20,26 @@ def index():
 @site.route('/login')
 @open_id.loginhandler
 def login():
-    return open_id.try_login('https://www.google.com/accounts/o8/id', \
-        ask_for=['email'])
+    return open_id.try_login('formstack.com', \
+        ask_for=['email', 'fullname'])
 
 @site.route('/logout')
 @login_required
 def logout():
-    chalicepoints.r.hdel('openid', current_user.url)
     logout_user()
     return redirect(url_for('site.index'))
 
 @site.route('/humans.txt')
 def humans():
-    return send_from_directory(os.path.join(app.root_path, 'public'),
+    return send_from_directory(os.path.join(current_app.root_path, 'public'),
         'humans.txt', mimetype='text/plain')
 
 @site.route('/robots.txt')
 def robots():
-    return send_from_directory(os.path.join(app.root_path, 'public'),
+    return send_from_directory(os.path.join(current_app.root_path, 'public'),
         'robots.txt', mimetype='text/plain')
 
 @site.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'public'),
+    return send_from_directory(os.path.join(current_app.root_path, 'public'),
         'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@site.route('/googleaf18a4916e849528.html')
-def gwt():
-    return send_from_directory(os.path.join(app.root_path, 'public'),
-        'googleaf18a4916e849528.html', mimetype='text/html')
