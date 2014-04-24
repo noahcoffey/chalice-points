14e4ca8b71e3c1d5939c5df83489d1a962ff198b
Brandon Peters <brandon@formstack.com>
2014-04-24 12:07:58 +0000

    Updated foundation ruby sass

--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
-require 'zurb-foundation'
-# Require any additional compass plugins here.
+add_import_path "chalicepoints/public/bower_components/foundation/scss"
 
+# Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
 http_path = "/"
