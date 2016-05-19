f1c701517677c4b2dba7113888324b630c9a6fb6
Brandon Peters <brandon.peters@gmail.com>
2016-05-19 15:51:21 -0400

    Add datepicker directive

--- chalicepoints/public/js/directives.js
+++ chalicepoints/public/js/directives.js
@@ -1,4 +1,25 @@
 (function() {
   'use strict';
-  angular.module('chalicepoints.directives', []);
- })();
+  angular.module('chalicepoints.directives', [])
+  .directive('datepicker', ['$parse', function($parse) {
+    return {
+      restrict: 'A',
+      require : 'ngModel',
+      link : function ($scope, $element, $attrs, ngModelCtrl) {
+        $(function() {
+          var handler = $parse($attrs.datepicker);
+
+          $element.datepicker({
+            dateFormat: 'yy-mm-dd',
+            onSelect: function (date) {
+              $scope.$apply(function () {
+                ngModelCtrl.$setViewValue(date);
+                handler();
+              });
+            }
+          });
+        });
+      }
+    }
+  }]);
+})();
