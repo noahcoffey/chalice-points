faeb9c71313d5e6c73eb403bacd9966394f76efe
Brandon Peters <brandon.peters@gmail.com>
2016-06-02 16:57:26 -0400

    Switched to two spaces

--- chalicepoints/__init__.py
+++ chalicepoints/__init__.py
@@ -1,5 +1,5 @@
 import os, sys, string
-import json
+import simplejson as json
 import hashlib
 import base64
 
@@ -15,14 +15,13 @@ from chalicepoints.exceptions import APIException, WebException
 
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 
-app = Flask(__name__, static_folder=os.path.join(PROJECT_ROOT, 'public'),
-        static_url_path='/public')
+app = Flask(__name__, static_folder=os.path.join(PROJECT_ROOT, 'public'), static_url_path='/public')
 
 app.config.from_object('chalicepoints.config.DefaultConfig')
 app.config['SECRET_KEY'] = os.getenv('APP_SECRET_KEY', app.config['SECRET_KEY'])
 
 if app.config['SECRET_KEY'] is None:
-    abort(500)
+  abort(500)
 
 # Flask-Login
 login_manager = LoginManager()
@@ -30,56 +29,34 @@ login_manager.login_view = 'site.login'
 login_manager.init_app(app)
 
 app.config['DATABASE'] = {
-    'engine': 'peewee.MySQLDatabase',
-    'name': app.config['MYSQL_DATABASE'],
-    'user': app.config['MYSQL_USER'],
-    'passwd': app.config['MYSQL_PASSWORD'],
-    'host': app.config['MYSQL_HOST'],
-    'threadlocals': True,
-    'autocommit': False
+  'engine': 'peewee.MySQLDatabase',
+  'name': app.config['MYSQL_DATABASE'],
+  'user': app.config['MYSQL_USER'],
+  'passwd': app.config['MYSQL_PASSWORD'],
+  'host': app.config['MYSQL_HOST'],
+  'threadlocals': True,
+  'autocommit': False
 }
 
 db = Database(app)
 
 def register_blueprint(app):
-    from chalicepoints.views.site import site
-    app.register_blueprint(site)
+  from chalicepoints.views.site import site
+  app.register_blueprint(site)
 
-    from chalicepoints.views.api import api
-    app.register_blueprint(api)
+  from chalicepoints.views.api import api
+  app.register_blueprint(api)
 
 register_blueprint(app)
 
 @login_manager.user_loader
 def load_user(id):
-    from chalicepoints.models.user import User
+  from chalicepoints.models.user import User
 
-    try:
-        return User.get(User.id == id)
-    except:
-        return None
-
-@login_manager.header_loader
-def load_user_from_header(header):
-    from chalicepoints.models.user import User
-
-    if header.startswith('Basic '):
-        header = header.replace('Basic ', '', 1)
-
-    try:
-        header = base64.b64decode(header)
-    except TypeError:
-        pass
-
-    header = header.split(':')[0]
-
-    if header == app.config['HIPCHAT_SECRET_KEY']:
-      return User()
-
-    try:
-        return User.get(User.api_key == header)
-    except:
-        return None
+  try:
+    return User.get(User.id == id)
+  except:
+    return None
 
 @app.after_request
 def after_request(response):
