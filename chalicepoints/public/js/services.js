035a7e566c6c40893a65ad5274ea3181c5575beb
Brandon Peters <brandon.peters@gmail.com>
2016-06-02 17:01:57 -0400

    Added new user endpoints. Switched to two spaces

--- chalicepoints/public/js/services.js
+++ chalicepoints/public/js/services.js
@@ -29,26 +29,40 @@
     'other'
   ])
   .factory('Leaderboard', function($resource) {
-      return $resource('/api/1.0/leaderboard/:type', {
-          type: 'all'
-      });
+    return $resource('/api/1.0/leaderboard/:type', {
+      type: 'all'
+    });
   })
   .factory('User', function($resource) {
-      return $resource('/api/1.0/user/:id', {
-          id: '@id'
-      }, {
-        update: {
-          method: 'PUT'
+    return $resource('/api/1.0/user/:id/:action', {
+      id: '@id'
+    }, {
+      targets: {
+        method: 'GET',
+        params: {
+          id: 'targets'
         },
-        merge: {
-          url: '/api/1.0/merge/:id/:targetId',
-          method: 'POST',
-          params: {
-            id: '@id',
-            targetId: '@target'
-          }
+        isArray: true
+      },
+      timeline: {
+        method: 'GET',
+        params: {
+          action: 'timeline'
+        },
+        isArray: true
+      },
+      update: {
+        method: 'PUT'
+      },
+      merge: {
+        url: '/api/1.0/merge/:id/:targetId',
+        method: 'POST',
+        params: {
+          id: '@id',
+          targetId: '@target'
         }
-      });
+      }
+    });
   })
   .factory('History', function($resource) {
     var History = $resource('/api/1.0/history/:type', {
@@ -67,39 +81,39 @@
     return History;
   })
   .factory('Timeline', function($resource) {
-      return $resource('/api/1.0/timeline', {});
+    return $resource('/api/1.0/timeline', {});
   })
   .factory('CPEvent', function($resource) {
-      return $resource('/api/1.0/event/:id', {
-          id: "@id"
-      }, {
-        update: {
-          method: 'PUT'
-        }
-      });
+    return $resource('/api/1.0/event/:id', {
+      id: "@id"
+    }, {
+      update: {
+        method: 'PUT'
+      }
+    });
   })
   .factory('flash', function($rootScope, $timeout) {
-      $rootScope.message = {};
+    $rootScope.message = {};
 
-      var reset;
-      var cleanup = function() {
-          $timeout.cancel(reset);
-          reset = $timeout(function() {
-              $('.alert-box').slideUp(400, function() {
-                  $rootScope.message = {};
-              });
-          }, 2000);
+    var reset;
+    var cleanup = function() {
+      $timeout.cancel(reset);
+      reset = $timeout(function() {
+        $('.alert-box').slideUp(400, function() {
+          $rootScope.message = {};
+        });
+      }, 2000);
+    };
+
+    return function(level, text) {
+      $rootScope.message = {
+        level: level,
+        text: text
       };
 
-      return function(level, text) {
-          $rootScope.message = {
-              level: level,
-              text: text
-          };
+      $('.alert-box').slideDown();
 
-          $('.alert-box').slideDown();
-
-          cleanup();
-      };
+      cleanup();
+    };
   });
 })();
