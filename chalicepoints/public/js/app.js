449c66153a68b150bc8d8b49b6c65189bf1f03b4
Brandon Peters <brandon.peters@gmail.com>
2016-06-02 17:00:40 -0400

    Refactored routes and app calls. Added controllers

--- chalicepoints/public/js/app.js
+++ chalicepoints/public/js/app.js
@@ -3,58 +3,46 @@
 
   $(document).foundation();
 
-  var routeConfig = {
-    defaultRoute: '/',
-    routes: {
-      '/': {
-        templateUrl: '/public/partials/leaderboard.html',
-        controller: LeaderboardCtrl,
-        resolve: LeaderboardCtrl.resolve
-      },
-      '/user/:id': { 
-        templateUrl: '/public/partials/profile.html',
-        controller: UserCtrl,
-        resolve: UserCtrl.resolve
-      },
-      '/history': {
-        templateUrl: '/public/partials/history.html',
-        controller: HistoryCtrl
-      },
-      '/users': {
-        templateUrl: '/public/partials/elder/users.html',
-        controller: ElderUsersCtrl,
-        resolve: ElderUsersCtrl.resolve
-      }
-    }
-  };
-
-  var app = angular.module('chalicepoints', [
+  angular.module('chalicepoints', [
     'ngRoute',
+    'ngAnimate',
+    'mm.foundation',
     'chalicepoints.services',
+    'chalicepoints.controllers',
     'chalicepoints.filters',
-    'chalicepoints.directives',
-    'mm.foundation'
-  ]);
-
-  app.config(['$routeProvider', '$locationProvider',
+    'chalicepoints.directives'
+  ])
+  .config(['$routeProvider', '$locationProvider',
     function($routeProvider, $locationProvider) {
-      if (routeConfig.routes !== undefined) {
-        angular.forEach(routeConfig.routes, function(route, path) {
-            $routeProvider.when(path, route);
+      $routeProvider
+        .when('/', {
+          templateUrl: '/public/partials/leaderboard.html',
+          controller: 'LeaderboardController',
+          controllerAs: 'leaderboardCtrl'
+        })
+        .when('/user/:id', {
+          templateUrl: '/public/partials/profile.html',
+          controller: 'UserController',
+          controllerAs: 'userCtrl'
+        })
+        .when('/history', {
+          templateUrl: '/public/partials/history.html',
+          controller: 'HistoryController',
+          controllerAs: 'historyCtrl'
+        })
+        .when('/users', {
+          templateUrl: '/public/partials/elder/users.html',
+          controller: 'ElderUsersController',
+          controllerAs: 'elderUsersCtrl'
+        })
+        .otherwise({
+          redirectTo: '/'
         });
-      }
-
-      if (routeConfig.defaultRoute !== undefined) {
-        $routeProvider.otherwise({
-          redirectTo: routeConfig.defaultRoute
-        });
-      }
 
       $locationProvider.html5Mode(true);
     }
-  ]);
-
-  app.run(['$rootScope', function($rootScope) {
+  ])
+  .run(['$rootScope', function($rootScope) {
     // Publish the user on the root scope.
     var user = document.getElementById('user');
     if (user) {
